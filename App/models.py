ba1949ba186836905b1abf95eb11234264e5fa37
JosiahBajnath <77852438+bhajicomp@users.noreply.github.com>
2024-03-06 17:52:34 -0400

    Updated the user model in the models file to include the properties id, username, email, and password, as well as the methods catch_pokemon, release_pokemon, rename_pokemon, set_password, check_password, and confirm.

--- App/models.py
+++ App/models.py
@@ -4,11 +4,40 @@ db = SQLAlchemy()
 
 class UserPokemon(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  pass
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'))
+  name = db.Column(db.String(50))
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  pass
+  username = db.Column(db.String(50), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(128))
+  pokemons = db.relationship('UserPokemon', backref='user', lazy=True)
+
+  def catch_pokemon(self, pokemon_id, name):
+    new_pokemon = UserPokemon(user_id=self.id, pokemon_id=pokemon_id, name=name)
+    db.session.add(new_pokemon)
+    db.session.commit()
+
+  def release_pokemon(self, pokemon_id, name):
+    pokemon = UserPokemon.query.filter_by(user_id=self.id, pokemon_id=pokemon_id, name=name).first()
+    if pokemon:
+      db.session.delete(pokemon)
+      db.session.commit()
+
+  def rename_pokemon(self, pokemon_id, name):
+    pokemon = UserPokemon.query.filter_by(user_id=self.id, pokemon_id=pokemon_id).first()
+    if pokemon:
+      pokemon.name = name
+      db.session.commit()
+
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+    db.session.commit()
+
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
 
 class Pokemon(db.Model):
   id = db.Column(db.Integer, primary_key=True)
